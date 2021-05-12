eb977f5c14a00249636f6250459b093059c62539
Jimmy Saavedra <jimmy.saavedra@bell.ca>
2021-05-12 14:00:46 -0400

    Removing code from main.go

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
+// https://tutorialedge.net/golang/creating-restful-api-with-golang/
+// go run main.go
+
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 type Person struct {
@@ -15,42 +16,19 @@ type Person struct {
 
 var people []Person
 
-func GetPeople(response http.ResponseWriter, request *http.Request) {
-	json.NewEncoder(response).Encode(people)
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "homePage EndPoint")
+  fmt.Println("Endpoint Hit: homePage")
 }
 
-func GetPerson(response http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	for _, item := range people {
-		if item.Id == params["id"] {
-			json.NewEncoder(response).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(response).Encode(&Person{})
-}
-
-func AddPerson(response http.ResponseWriter, request *http.Request) {
-	//json.NewEncoder(response).Encode(people)
-}
-
-func DeletePerson(response http.ResponseWriter, request *http.Request) {
-	//json.NewEncoder(response).Encode(people)
-}
-
-func main() {
-	router := mux.NewRouter()
-
-	people = append(people, Person{Id: "1", FirstName: "Liz", LastName: "Bur"})
-	people = append(people, Person{Id: "2", FirstName: "Jim", LastName: "Saa"})
-
-	//End-Points
-	router.HandleFunc("/people", GetPeople).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	router.HandleFunc("/people{id}", AddPerson).Methods("POST")
-	router.HandleFunc("/people{id}", DeletePerson).Methods("DELETE")
+func handleRequests() {
+	http.HandleFunc("/", homePage)
 
 	//Star web server and log
 	//log.Fatal(http.ListenAndServe(":3000", router))
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":1000", nil)
+}
+
+func main() {
+		handleRequests()
 }

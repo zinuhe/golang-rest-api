808c7f4d0bc2429ca8f60f62a0f8fad6a8d41f15
Jimmy Saavedra <jimmy.saavedra@bell.ca>
2021-05-12 14:12:19 -0400

    Fixing spaces and tabs

--- main.go
+++ main.go
@@ -4,31 +4,31 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+  "fmt"
+  "net/http"
 )
 
 type Person struct {
-	Id        string `json:"id,omitempty"`
-	FirstName string `json:"firstname,omitempty"`
-	LastName  string `json:"lastname,omitempty"`
+  Id        string `json:"id,omitempty"`
+  FirstName string `json:"firstname,omitempty"`
+  LastName  string `json:"lastname,omitempty"`
 }
 
 var people []Person
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "homePage EndPoint")
+  fmt.Fprintf(w, "homePage EndPoint")
   fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+  http.HandleFunc("/", homePage)
 
-	//Star web server and log
-	//log.Fatal(http.ListenAndServe(":3000", router))
-	http.ListenAndServe(":1000", nil)
+  //Star web server and log
+  //log.Fatal(http.ListenAndServe(":3000", router))
+  http.ListenAndServe(":1000", nil)
 }
 
 func main() {
-		handleRequests()
+  handleRequests()
 }
